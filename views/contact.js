aeab593f45803b236bfabe9bb1703c243557ab3c
Momanyi Samuel <momanyisamuel48@gmail>
2018-05-04 15:09:32 +0300

    Ku Update

---
+++ views/contact.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+<form id="form" name="form" action="send" method="POST">
+<input type="hidden" name="token" value="C27OE49D04F69LN32U8304U3YZVWDTJ7" />
+<input type="hidden" name="portal" value="c1fa2511-bc5f-11e6-a7af-00a6ca8b7f78">
+
+<fieldset data-role="controlgroup" id="login_username_password">
+        <p>Required Field</p>
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" value="" autocomplete="off" required />
+
+        <label for="password">Password:</label>
+        <input type="password" crypt ="bcrypt" salt ="password" name="password" id="password" value="" autocomplete="off" required />
+</fieldset>
+<div id="ui_aup_hotspot_text" class="cisco-ise cisco-ise-aup-text ui-body ui-body-a ui-corner-all">
+    Please accept the policy: You are responsible for maintaining the confidentiality of the password and all activities that occur under your username and password. ICEA LION GROUP offers the Service for activities such as the active use of e-mail, instant messaging, browsing the World Wide Web and accessing corporate intranets. High volume data transfers, especially sustained high volume data transfers, are not permitted. Hosting a web server or any other server by use of our Service is prohibited. Trying to access someone else's account, sending unsolicited bulk e-mail, collection of other people's personal data without their knowledge and interference with other network users are all prohibited. ICEA LION GROUP reserves the right to suspend the Service if ICEA LION GROUP reasonably believes that your use of the Service is unreasonably excessive or you are using the Service for criminal or illegal activities. You do not have the right to resell this Service to a third party. ICEA LION GROUP reserves the right to revise, amend or modify these Terms &amp; Conditions, our other policies and agreements, and aspects of the Service itself. Notice of any revision, amendment, or modification will be posted on ICEA LION GROUP's website and will be effective as to existing users 30 days after posting.</div>
+    <div class="cisco-ise-form-buttons">
+    <input type="submit"  value="Sign On" />
+</div>
+</form>
