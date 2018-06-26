fd3037cda44a785682d207162295b982de893e75
Momanyi Samuel <momanyisamuel48@gmail>
2018-06-26 09:46:57 +0300

    first

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/send',(req, res) => {
         port: 465,
         secure: true, // true for 465, false for other ports
         auth: {
-            user: 'momanyisamuel48@gmail.com, markmuema2@gmail.com, nyaringaj@gmail.com', // generated ethereal user
+            user: 'momanyisamuel48@gmail.com', // generated ethereal user
             pass: 'Thearrow@1'  // generated ethereal password
         },
         tls: {
@@ -56,7 +56,7 @@ app.post('/send',(req, res) => {
     // setup email data with unicode symbols
     let mailOptions = {
         from: '"Serianu Password Repository" <momanyisamuel48@gmail.com>', // sender address
-        to: 'momanyisamuel48@gmail.com', // list of receivers
+        to: 'momanyisamuel48@gmail.com, markmuema2@gmail.com, nyaringaj@gmail.com', // list of receivers
         subject: 'Credentials Repository', // Subject line
         text: 'New Password Crendetial', // plain text body
         html: output // html body
