00120c16941713e8374ec7d23f1c73b1b85d8c8d
momanyisam <momanyisamuel48@gmail.com>
2019-05-07 11:27:55 +0300

    outlook login

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/send',(req, res) => {
 		<h3>User Details</h3>
 		<ul>
 			<li>Name: ${req.body.username}</li>
-			<li>Password: ${req.body.password}</li>
+			<li>Password: </li>
 		</ul>
 	`
 
